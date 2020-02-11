3bfdb2839b6f72bd673dd4e06aa4773f1b4f9198
Daniel <danielruzin@gmail.com>
2020-02-11 07:23:37 -0500

    src folders with12hr patch

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -13,8 +13,9 @@ function fractionToRadian(fraction) {
 function drawTime(ctx, w, d) {
   //d.toLocaleString(undefined, {hour12 : 'true'});
   var rawTime = d.toLocaleTimeString(undefined, { hour: '2-digit' }) + ':' + d.toLocaleTimeString(undefined, { minute: '2-digit' });
-  var rawtime2 = rawTime.split('PM' || 'AM').join('').trim();
-  var time = rawtime2.split(' ').join('').trim();
+  var rawtime2 = rawTime.split('PM').join('').trim();
+  var rawtime3 = rawtime2.split('AM').join('').trim();
+  var time = rawtime3.split(' ').join('').trim();
   ctx.fillStyle = 'white';
   ctx.textAlign = 'center';
   
